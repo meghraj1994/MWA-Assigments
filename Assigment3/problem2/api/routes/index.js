04c09d84e27fdb4936f98bee9d019a5f85968394
meghraj1994 <meghrajprasadk@gmail.com>
2021-09-30 09:29:16 -0500

    renamed problem2

--- Assigment3/problem2/api/routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controller/controller');
-
-router.route('/numbers/:paramNum').get(controller.multFunction);
-
-module.exports = router;
